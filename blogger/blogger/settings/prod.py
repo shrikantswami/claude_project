16b260d41c76347228531bc62689258bac42ecce
shrikant <shrikantswami2011@gmail.com>
2026-07-03 01:12:37 +0530

    updated settings file

--- blogger/blogger/settings/prod.py
+++ blogger/blogger/settings/prod.py
@@ -22,9 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
-ALLOWED_HOSTS = os.environ['ALLOWED_HOSTS'].split(',')
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY', 'fallback-dev-key-change-in-prod')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
