9a8a9daa0156030d9242b7dc6ec15599065a5db4
shrikant <shrikantswami2011@gmail.com>
2026-07-03 23:33:57 +0530

    CSRF_TRUSTED_ORIGINS

--- blogger/blogger/settings/prod.py
+++ blogger/blogger/settings/prod.py
@@ -25,7 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'fallback-dev-key-change-in-prod')
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
 
-
+CSRF_TRUSTED_ORIGINS = [
+    'https://claudeproject-production-9384.up.railway.app',
+]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
