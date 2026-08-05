871c768ef7b4902dcefd311742d24b060412a4ff
shrikant <shrikantswami2011@gmail.com>
2026-08-05 23:50:31 +0530

    BASE_DIR updated

--- blogger/blogger/settings/prod.py
+++ blogger/blogger/settings/prod.py
@@ -10,7 +10,7 @@ import dj_database_url
 # ══════════════════════════════════════
 # Base directory
 # ══════════════════════════════════════
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # ══════════════════════════════════════
